114ee9053e19a612e56323da286b01e064c00de8
Tasawwur-Mehnaz <73061366+Tasawwur-Mehnaz@users.noreply.github.com>
2022-07-19 22:48:31 +0530

    Greedy, array

--- Candy.cpp
+++ Candy.cpp
@@ -14,7 +14,8 @@ Input: ratings = [1,2,2]
 Output: 4
 Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
 The third child gets 1 candy because it satisfies the above two conditions.
-
+    
+    
 
 Solution:
 
